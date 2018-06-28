f2c7dc13cea5391e87f5f2510f615c90fe89faa8
RogiTG <rogitg@interia.pl>
2018-06-28 19:54:22 +0200

    Json pierwszy commit zad 5

---
+++ src/main/java/pl/sdacademy/tournament/group/WorldCup2018JSON.java
@@ -0,0 +1,11 @@
+package pl.sdacademy.tournament.group;
+
+import jdk.nashorn.internal.runtime.JSONFunctions;
+import org.json.JSONObject;
+
+public class WorldCup2018JSON {
+
+JSONObject jsonObject = new JSONObject();
+
+
+}
