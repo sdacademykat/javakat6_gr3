92c4de36a835ba142d99c4fcb12828379924884f
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-06-28 19:54:50 +0200

    Task 6

---
+++ src/main/java/pl/sdacademy/tournament/group/providers/JsonGroupPhaseDataProvider.java
@@ -0,0 +1,15 @@
+package pl.sdacademy.tournament.group.providers;
+
+import pl.sdacademy.tournament.group.GroupPhase;
+import pl.sdacademy.tournament.group.GroupPhaseDataProvider;
+
+/**
+ * Created by Mateusz Grabarski on 28.06.2018.
+ */
+public class JsonGroupPhaseDataProvider implements GroupPhaseDataProvider {
+    
+    @Override
+    public GroupPhase getGroupPhase() {
+        return null;
+    }
+}
